0902a69ae1067e39ac24e3e13675f3c94e9a536a
melki <melkishitesfayeangassa@gmail.com>
2024-08-21 12:11:41 +0300

    Finish driver side UI

--- mobile/driver/lib/core/routes/router_config.dart
+++ mobile/driver/lib/core/routes/router_config.dart
@@ -1,6 +1,6 @@
 import 'package:driver/core/routes/app_path.dart';
 import 'package:driver/features/authentication/presentation/pages/login_page.dart';
-import 'package:driver/features/authentication/presentation/pages/tracking_page.dart';
+import 'package:driver/features/bus_tracking/presentation/pages/tracking_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
