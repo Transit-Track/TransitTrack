f3ae1bc2cecd50fc13ae7d5ec194f0e343e796bb
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-08-29 20:33:56 +0300

    "restructure folder"

--- app/db/mongodb.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from app.core.config import settings
-
-client = AsyncIOMotorClient(settings.MONGODB_URL)
-database = client[settings.MONGODB_NAME]
-
-def get_database():
-    return database
