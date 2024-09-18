16a716b65c1a86f2aec29102ec696b0206ef6ec6
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-18 17:33:14 +0300

    Removed unnecessary files

--- mobile/lib/features/driver/domain/repository/driver_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:transittrack/core/error/failure.dart';
-
-abstract class DriverRepository {
-  Future<Either<Failure, String>> updateDriverLocation({
-    required double latitude,
-    required double longitude,
-  });
-}
